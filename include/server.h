fae3c01b728c85ea12d5559983ec7122fedb7551
Junhong Wang <oneone@g.ucla.edu>
2020-04-09 06:46:36 +0000

    Refactor the echo server code
    We want to implement HTTP web server later,
    but the skeleton code for the echo server from
    previous commit had everything inside server_main.cc
    
    Separate classes into their own files so that
    the code is cleaner.
    
    Add a simple test case ServerEchoesMessageBackToClient
    to check the echo server's behavior is indeed
    not changed.
    
    Change-Id: Ibc5e36c01644eeb39dd7de2d07fabd70cde21d3b

---
+++ include/server.h
@@ -0,0 +1,25 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+class session;
+
+class server
+{
+public:
+  server(boost::asio::io_service& io_service, short port);
+
+private:
+  void start_accept();
+  void handle_accept(session* new_session,
+      const boost::system::error_code& error);
+ 
+  boost::asio::io_service& io_service_;
+  tcp::acceptor acceptor_;
+};
+
+#endif
