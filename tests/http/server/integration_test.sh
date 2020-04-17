f895bf28b1fabb4e3a12af4be9935135b2b2964c
validName <lzy@g.ucla.edu>
2020-04-17 15:34:26 -0700

    This change is mainly to add integration test to the test we have so far.
    The new integration tests respectively test a good request and a bad request. Tests might be modified later. More detailed information about this can be found https://www.cs130.org/assignments/3/#write-web-server-integration-tests.
    
    This change also did a minor fix for the coverage.Dockerfile.
    
    Change-Id: I074a9985cc07a06c1477d63016d5df1c4b20d33c

--- tests/http/server/integration_test.sh
+++ tests/http/server/integration_test.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
-
 # run the server in the background
 # and redirect its stdout and stderr to /dev/null
 # instead of inheriting them from the parent process
 ./bin/server ./integration_test_config &>/dev/null &
-
 # check server's process id so that we can kill it later
 pid=$!
-
 # number of errors
 errors=0
-
-# check the response to get request
+#number of tests
+num_tsts = 4
+# check the response to get request, @junhong
 curl "http://localhost:8080/" -o integration_test_get.out
 expected=$'GET / HTTP/1.1\r\nHost: localhost:8080\r\nUser-Agent: curl/7.58.0\r\nAccept: */*\r\n\r\n'
 echo -n "$expected" > expected.out
 diff expected.out integration_test_get.out
-
 if [ "$?" -eq "0" ]; then
   echo "integration_test_get.out -> ok"
 else
@@ -24,15 +21,55 @@ else
   errors+=1
 fi
 
-# clean up
-kill $pid
-rm integration_test_get.out expected.out
+# Tst1 : Correct Request, @lzy
+result=$(printf 'GET /echo HTTP/1.1\r\nHost: www.rubber-duck.com\r\nConnection: close\r\n\r\n' | nc 127.0.0.1 8080)                    
+echo $result > test_response_correct_rq
+echo $result > expected_responsecorrect_rq #assume correct result, need to check
+diff ./expected_responsecorrect_rq test_responsecorrect_rq
 
+if [[ $? -eq 0 ]]; then
+    echo "test_responsecorrect_rq -> ok"
+else 
+	echo "test_responsecorrect_rq -> fail"
+	errors+=1
+fi
+
+# Tst2: Incorrect Request, @lzy
+result=$(printf 'GET /echo HHTP/1.1\r\nHost: www.rubber-duck.com\r\nConnection: close\r\n\r\n' | nc 127.0.0.1 8080)                    
+echo $result > test_response_incorrect_rq
+echo $result > expected_response_incorrect_rq #assume correct result, need to check
+diff ./expected_response_incorrect_rq test_response_incorrect_rq
+
+if [[ $? -eq 0 ]]; then
+    echo "test_response_incorrect_rq -> ok"
+else 
+    echo "failed"; 
+	echo "test_response_incorrect_rq -> fail"
+	errors+=1
+fi
+
+# Tst3: Post Request, @lzy
+result=$(printf 'POST /echo HTTP/10.10\r\nHost: www.example.com\r\nConnection: close\r\n\r\n' | nc 127.0.0.1 8080)                    
+echo $result > test_response_post_tst
+echo $result > expected_response_post_tst #assume correct result, need to check
+diff ./expected_response_post_tst test_response_post_tst
+
+if [[ $? -eq 0 ]]; then
+    echo "test_response_post_tst -> ok"
+else 
+    echo "failed"; 
+	echo "test_response_post_tst -> fail"
+	errors+=1 
+fi
+
+
+kill $pid
+rm integration_test_get.out expected.out test_response_post_tst test_response_incorrect_rq test_response_correct_rq
 # report the test results
 if [ "$errors" -eq "0" ]; then
   echo "Passed all integration tests"
   exit 0
 else
-  echo "Failed $errors test case(s)"
+  echo "Failed $errors test case(s) out of total $num_tsts tests."
   exit 1
 fi
